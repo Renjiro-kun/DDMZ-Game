6cdc260a150c575974cf375184bacb5f51d7b1a1
Renjiro-kun <48997659+Renjiro-kun@users.noreply.github.com>
2024-11-15 17:46:24 +0100

    Fixed bug with maze generatiorn; cleaned up generation code a bit

--- src/MazeGenerator.h
+++ src/MazeGenerator.h
@@ -7,29 +7,31 @@
 class MazeGenerator
 {
 public:
-    static void GenerateMazeMap(const std::string& name, Mesh& maze);
+	static void GenerateMazeMap(const std::string& name, Mesh& maze);
 private:
-    enum LayerType : char
-    {
-        Data = 0
-    };
+	enum LayerType : char
+	{
+		Data = 0
+	};
 
-    struct LayerInfo
-    {
-        std::string name;
-        LayerType type;
-        void* data;
-        size_t dataSize;
-    };
+	struct LayerInfo
+	{
+		std::string name;
+		LayerType type;
+		void* data;
+		size_t dataSize;
+	};
 
-    struct MazeInfo
-    {
-        char width = 0;
-        char height = 0;
-        std::vector<LayerInfo> layers;
-    };
+	struct MazeInfo
+	{
+		char width = 0;
+		char height = 0;
+		std::vector<LayerInfo> layers;
+	};
 private:
-    static void ParseFile(std::ifstream& file, MazeGenerator::MazeInfo& maze);
+	static void ParseFile(std::ifstream& file, MazeGenerator::MazeInfo& maze);
 
-    static void GenerateMesh(MazeGenerator::MazeInfo& info, Mesh& outMesh);
+	static void GenerateMesh(MazeGenerator::MazeInfo& info, Mesh& outMesh);
+
+	static bool CheckLayerData(void* data, size_t idx, char value);
 };
